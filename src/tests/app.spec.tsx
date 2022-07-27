03e9f125eac751f1b4b1a2ec72487778c41e5097
diasraphael <diasraphael88@gmail.com>
2022-07-27 11:49:43 +0200

    Added test cases for add todo

--- src/tests/app.spec.tsx
+++ src/tests/app.spec.tsx
@@ -1,7 +1,8 @@
 import App from "../App";
-import { screen } from "@testing-library/react";
+import { screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import { renderWithProviders } from "../test-utils";
+import TodoApp from "../components/AddTodo";
 
 describe("TodoApp", () => {
   const initialState = {
@@ -30,9 +31,25 @@ describe("TodoApp", () => {
     const buyMilkTodo = screen.queryByTestId("done0");
     expect(buyMilkTodo).toBeNull();
 
-    //TODO: Verify second todo
+    // Verify second todo
     expect(screen.getByText("Buy bread")).toBeDefined();
     const buyBreadTodo = screen.getByTestId("done1");
     expect(buyBreadTodo).toBeDefined();
+
+    expect(initialState.todoList).toHaveLength(2);
   });
+
+  it("checks the inital state of the Add todo component", () => {
+    const app = renderWithProviders(<TodoApp />);
+    const addTodo = screen.getByRole("button", { name: "Add Todo" });
+    expect(addTodo).toBeInTheDocument();
+    expect(addTodo).toBeDisabled();
+  });
+
+  /* it("checks the click functionality of the Add todo component", () => {
+    const app = renderWithProviders(<TodoApp />);
+    const addTodoInput = screen.getByRole("input");
+    fireEvent.change(addTodoInput);
+    expect(addTodoInput.textContent).toBeTruthy();
+  }); */
 });
